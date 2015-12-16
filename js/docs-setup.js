deb4fa5ee811b516944ebd875350cfbccf427c74
Will Jones <the8bitsquid@gmail.com>
2015-12-16 17:52:49 -0600

    Updates

---
+++ js/docs-setup.js
@@ -0,0 +1,70 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "The Music Library&#39;s Videos search application.",
+      "keywords": "api application lib library music overview route search ua url videos"
+    },
+    {
+      "section": "api",
+      "id": "videos",
+      "shortName": "videos",
+      "type": "overview",
+      "moduleName": "videos",
+      "shortDescription": "The Music Library&#39;s Videos search application.",
+      "keywords": "angular-filter api application duscroll lib library music nganimate ngresource ngroute ngsanitize overview route search ua ualib-ui ui-bootstrap url videos"
+    },
+    {
+      "section": "api",
+      "id": "videos.Controller:VideosListCtrl",
+      "shortName": "VideosListCtrl",
+      "type": "controller",
+      "moduleName": "videos",
+      "shortDescription": "Controller for the videos route.",
+      "keywords": "$document $filter $location $scope api auto-scroll bind controller default exposed filters method navigating pagechange params reset resetfilters resolve route top triggered uri values videos"
+    },
+    {
+      "section": "api",
+      "id": "videos.filters",
+      "shortName": "videos.filters",
+      "type": "service",
+      "moduleName": "videos",
+      "shortDescription": "Gets filters available for the videos search UI",
+      "keywords": "api filters search service ui videos"
+    },
+    {
+      "section": "api",
+      "id": "videos.videos",
+      "shortName": "videos.videos",
+      "type": "service",
+      "moduleName": "videos",
+      "shortDescription": "Gets the list of videos available in the Music Library",
+      "keywords": "api library list music service videos"
+    },
+    {
+      "section": "api",
+      "id": "videos.videosFactory",
+      "shortName": "videos.videosFactory",
+      "type": "service",
+      "moduleName": "videos",
+      "shortDescription": "Angular Service to call musicSearch API",
+      "keywords": "$promise $resource angular api call data musicsearch ng object params pass promise return service showall videos videosfactory"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "api/",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
